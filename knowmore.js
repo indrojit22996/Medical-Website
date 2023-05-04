cc5901bb799ff3909120433ec8658a66facc5ebb
indrojit22996 <indrojit22996bhaduri@gmail.com>
2023-05-04 19:02:26 +0530

    Update

--- knowmore.js
+++ knowmore.js
@@ -1,14 +1,17 @@
 const seeMore = document.querySelector(".seemore-btn");
 
 const seeLess = document.querySelector(".seeless-btn");
-
+const testiomnialHidden=document.querySelector(".testimonial-hidden-section")
 seeMore.addEventListener("click", () => {
-
+ 
+  testiomnialHidden.classList.remove("hidden-section");
  seeLess.classList.remove("see-hidden");
  seeMore.classList.add("see-hidden");
+ 
 });
 seeLess.addEventListener("click",()=>{
- 
+  testiomnialHidden.classList.add("hidden-section");
   seeLess.classList.add("see-hidden");
   seeMore.classList.remove("see-hidden");
 });
+
